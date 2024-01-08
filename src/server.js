e8956466ef703f4336acee94ac2d5cf79a20ffd7
NoraSaffar <norasaffar8910@gmail.com>
2024-01-08 10:26:58 +0000

    enhancement: add basic server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+
+const port = process.env.PORT || 5001;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.status(200).json({message: "API is healthy"})
+  
+  });
+
+
+app.listen(port, () =>{
+  console.log(`App is listening on port ${port}`);
+});
